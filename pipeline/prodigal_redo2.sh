fb0489c96fbab6a2ded8c27028b4ee15e6fdfa99
jwuw001 <103218589+jwuw001@users.noreply.github.com>
2022-12-09 19:52:32 -0800

    Delete prodigal_redo2.sh
    Wrong script - ran with -p anon instead of meta. -p anon only works with the more recent versions of prodigal and the cluster has an older version installed

--- pipeline/prodigal_redo2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash -l
-#sbatch -p short -N 1 -n 1 -c 32 --mem 128gb --job-name="PRODIGAL2r" --out logs/PDG.2r.%a.log
-
-#healthy-SRA SRR12131164
-module load prodigal
-prodigal -i ~/bigdata/GEN220_finalproject/analysis/megahit_result2/final.contigs2.fna -o coords_healthy.gbk -a proteins_healthy.faa -p anon
